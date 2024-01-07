cfe01a13e360bdcc90a25dd43285cd423798b56c
Brendon Geils <14355901+bgeils@users.noreply.github.com>
2024-01-07 17:04:32 -0500

    "wss://tldraw-yjs-example-pink.vercel.app"

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useYjsStore } from "./useYjsStore";
 const HOST_URL =
     import.meta.env.MODE === "development"
         ? "ws://localhost:1234"
-        : "wss://demos.yjs.dev";
+        : "wss://tldraw-yjs-example-pink.vercel.app";
 
 export default function YjsExample() {
     const store = useYjsStore({
